4deab6fea2cd4f58a435d97cf1405465ad1f1e70
Adina Howe <howead@msu.edu>
2014-10-23 16:07:05 -0500

    added scripts used for Googling in amazing ways

---
+++ scripts/parse-the-giant-excel.py
@@ -0,0 +1,10 @@
+import sys
+
+for n, line in enumerate(open(sys.argv[1], 'rU')):
+    if n < 2:
+        continue
+    else:
+        dat = line.rstrip().split('\t')
+        if len(dat) == 36:
+            strain = dat[9]
+            print strain
